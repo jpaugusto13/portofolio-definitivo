b392bb630a14c20db363166f7b6e39d86b4ad686
jpaugusto13 <jpaugusto13@proton.me>
2026-03-02 08:28:05 -0300

    Portfolio

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "../App";
 import Portfolio from "../Estampas";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/" element={<Portfolio />} />
       </Routes>
     </BrowserRouter>
   );
